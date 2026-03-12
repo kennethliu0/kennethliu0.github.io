60d9a29b807ebf02fbfd538d7b82b6ee31e83087
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2026-03-12 09:47:07 -0700

    improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kennethliu.me",
+  integrations: [sitemap()],
   image: {
     domains: ["cdn.jsdelivr.net"],
   },
